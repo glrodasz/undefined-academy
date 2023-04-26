c4a1014ca7aa2d568325bb24453da20560561851
Guillermo Rodas <me@guillermorodas.com>
2023-04-26 06:35:43 +0200

    Cards from array

--- intro-a-react/src/App.jsx
+++ intro-a-react/src/App.jsx
@@ -3,13 +3,30 @@ import { Card } from "./Card";
 
 class App extends Component {
   render() {
-    // const articles = [{}, {}, {}]
+    const articles = [
+      {
+        title: "Oi Mundo",
+        text: "Como vai?",
+      },
+      {
+        title: "Hello",
+        text: "How are you?",
+      },
+      {
+        title: "Hello",
+        text: "How are you",
+      },
+      {
+        title: "Hej",
+        text: "Värld",
+      },
+    ];
 
     return (
       <>
-        <Card title="Oi Mundo">Como vai?</Card>
-        <Card title="Hello">How are you</Card>
-        <Card title="Hello">How are you</Card>
+        {articles.map(({ title, text }) => (
+          <Card title={title}>{text}</Card>
+        ))}
       </>
     );
   }
